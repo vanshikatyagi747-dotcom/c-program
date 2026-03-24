31dc2653b9f786dc99193712028ba9f4bee9d434
Vanshika tyagi <vanshikatyagi747@gmail.com>
2026-03-24 18:01:03 +0530

    basic

---
+++ condition.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int x = 27, y = 25;
+    if (x > y)
+    {
+        printf("The Value of X is Greater \n");
+        printf("Ranjita ");
+    }
+
+    else
+
+        printf("The Value of Y is Greater \n");
+
+    printf("The End");
+
+    return 0;
+}
